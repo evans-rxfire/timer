50e8e999a82da4a14ace0a0ab6965151c0043f80
Wesley Evans <162752827+evans-rxfire@users.noreply.github.com>
2025-12-10 18:55:42 -0600

    2025.12.10 Added timer display to show time in HTML and set initial styling. Started work on JS framework.

---
+++ main.js
@@ -0,0 +1,16 @@
+const timerContainer = document.getElementById("timer-container");
+const displayContainer = document.getElementById("display-container");
+const timerDisplay = document.getElementById("timer-display");
+
+const userTimerLength = document.getElementById("set-timer-length");
+const userBreakLength = document.getElementById("set-break-length");
+
+const startBtn = document.getElementById("start-button");
+const stopBtn = document.getElementById("stop-button");
+const resetBtn = document.getElementById("reset-button");
+
+
+// Functions
+
+
+// Event listeners
